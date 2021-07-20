2960db0627c6348d28c46c522888f5fda42ea1b2
Aruna Wankhade <wankhadearuna098@gmail.com>
2021-07-20 17:39:43 +0530

    #3 : Implemented login page redirection

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, NgModule, OnInit, ViewChild} from '@angular/core';
+import {TabsPage} from '../tabs/tabs.page';
+import {RouterModule, Routes} from '@angular/router';
+import {AlertController, NavController, NavParams} from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
 
-  constructor() { }
+export class LoginPage implements OnInit {
+  @ViewChild('username') username;
+  @ViewChild('password') password;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController,public navParams: NavParams) {
+  }
 
   ngOnInit() {
   }
+
+  //
+  // login(){
+  //   // eslint-disable-next-line eqeqeq
+  //   if (this.username.value == 'admin' && this.password.value =='admin'){
+  //     this.router.navigate(['/detail'])
+  //   }else{
+  //     let alert = this.alertCtrl.create({
+  //       title: 'Please use  username :admin     password:admin',
+  //       buttons: ['Dismiss']
+  //     });
+  //     alert.present();
+  //   }
+  // }
 
 }
