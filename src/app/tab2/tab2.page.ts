96891155939ce4d133241a1f63e95194935661eb
= <=>
2021-07-22 04:47:14 +0530

    #5 : Implemented dynamic camera information cards

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  products: any;
 
+  constructor() { }
+
+  ngOnInit() {
+    fetch('./assets/json/products.json').then(res => res.json())
+    .then(json => {
+      this.products = json;
+    });
+  }
 }
